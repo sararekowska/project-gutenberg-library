6af4c2fc1f09028504446d9e8866d5e2dda90910
sararekowska <sara.rekowska@gmail.com>
2022-05-25 21:34:21 +0200

    style: desktop styles

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,7 +2,7 @@ const Sidebar = () => {
   return (
     <section className="w-full bg-[#F1AE82] xl:h-screen xl:w-[26rem]">
       {/* logo and text  */}
-      <div className="w-full flex items-center justify-evenly sm:justify-center">
+      <div className="w-full flex items-center justify-evenly sm:justify-center xl:pl-6 xl:pt-4">
         <img
           src="/images/book-logo.png"
           alt="library logo"
@@ -12,13 +12,13 @@ const Sidebar = () => {
       </div>
 
       {/* search fields  */}
-      <div className="w-full pb-5 mt-4 flex flex-col items-center">
+      <div className="w-full pb-5 mt-4 flex flex-col items-center xl:mt-8">
         {/* search by name  */}
         <div className="flex flex-col mb-4">
           <span className="text-lg">search by name:</span>
           <input
             type="text"
-            className="bg-[#D9D9D9] focus:outline-none pl-1 w-80"
+            className="bg-[#D9D9D9] focus:outline-none pl-1 w-80 xl:w-72"
           />
         </div>
 
@@ -27,14 +27,14 @@ const Sidebar = () => {
           <span className="text-lg">filter by genre:</span>
           <input
             type="text"
-            className="bg-[#D9D9D9] focus:outline-none pl-1 w-80"
+            className="bg-[#D9D9D9] focus:outline-none pl-1 w-80 xl:w-72"
           />
         </div>
       </div>
 
       {/* favourites  */}
       <div className="w-full flex justify-center pb-6">
-        <span className="text-lg w-[320px] underline">
+        <span className="text-lg w-[320px] underline xl:xl:w-72">
           my favourites {">>"}
         </span>
       </div>
