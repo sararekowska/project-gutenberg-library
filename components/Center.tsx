cfe47d005be49dce8ba5167f51e87e4bc25c2cfc
sararekowska <sara.rekowska@gmail.com>
2022-05-26 22:02:15 +0200

    feat: intersection observer

--- components/Center.tsx
+++ components/Center.tsx
@@ -1,10 +1,23 @@
+import { useEffect, useRef } from "react";
+import { useIntersection } from "react-use";
 import useListBooks from "../common/hooks/useListBooks";
 import BookItem from "./BookItem";
 import Spinner from "./Spinner";
 
 const Center = () => {
-  const { data, fetchNextPage, isLoading, books } = useListBooks();
-  console.log(books);
+  const intersectionRef = useRef(null);
+  const intersection = useIntersection(intersectionRef, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1,
+  });
+
+  useEffect(() => {
+    intersection?.isIntersecting && fetchNextPage();
+  }, [intersection?.isIntersecting]);
+
+  const { fetchNextPage, isLoading, books } = useListBooks();
+
   return (
     <section className="h-full w-full pt-8 xl:overflow-auto xl:w-[75%] xl:float-right xl:h-screen 2xl:w-[80%] scrollbar-hide">
       <div className="flex flex-col items-center">
@@ -13,18 +26,16 @@ const Center = () => {
         </span>
         <span className="text-xl xl:text-2xl mb-10">Available books:</span>
       </div>
-
       {/* books  */}
-      {isLoading ? (
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {books?.map((book) => (
+          <BookItem key={book.id} book={book} />
+        ))}
+      </div>
+
+      <div ref={intersectionRef}>
         <Spinner />
-      ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {books?.map((book) => (
-            <BookItem key={book.id} book={book} />
-          ))}
-        </div>
-      )}
-      <button onClick={() => fetchNextPage()}>x</button>
+      </div>
     </section>
   );
 };
